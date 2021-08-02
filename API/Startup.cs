db3ae96c62d4cecb29c30cac4602471c9566fa2a
Stanisław Franczyk <248857@student.pwr.edu.pl>
2021-08-02 23:25:30 +0200

    Add ssl and bootstrap

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,8 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -52,10 +54,13 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+
                 endpoints.MapControllers();
             });
         }
