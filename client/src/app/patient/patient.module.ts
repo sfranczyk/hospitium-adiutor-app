9530bf0eb6bd499b1aaf6bdd8e09a483b09ac1a7
Stanisław Franczyk <248857@student.pwr.edu.pl>
2021-10-25 19:30:22 +0200

    Add Patient module

---
+++ client/src/app/patient/patient.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PatientRoutingModule } from './patient-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PatientRoutingModule
+  ]
+})
+export class PatientModule { }
